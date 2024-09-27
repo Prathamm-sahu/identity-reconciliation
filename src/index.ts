7d01efa364a9c4db4fbf34c8de1d0c56c0ae0f89
Pratham Sahu <prathamsahu051@gmail.com>
2024-09-27 14:00:54 +0530

    formatted file

--- src/index.ts
+++ src/index.ts
@@ -1,246 +1,265 @@
-import express, { Request, Response } from "express"
-import cors from "cors"
-import dotenv from "dotenv"
-import prisma from "./lib/db"
-dotenv.config()
-const PORT = process.env.PORT || 3000
+import express, { Request, Response } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import prisma from "./lib/db";
+dotenv.config();
+const PORT = process.env.PORT || 3000;
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 app.post("/api/identity", async (req: Request, res: Response) => {
   try {
-    const { email, phoneNumber } = req.body
-    console.log(email, phoneNumber)
+    const { email, phoneNumber } = req.body;
+    console.log(email, phoneNumber);
 
     const contacts = await prisma.contact.findMany({
       where: {
         OR: [
           {
-            email
+            email,
           },
           {
-            phoneNumber
-          }
-        ]
+            phoneNumber,
+          },
+        ],
       },
-    })
-    console.log(contacts)
+    });
+    console.log(contacts);
 
     // No contact Exists
-    if(contacts.length === 0) {
+    if (contacts.length === 0) {
       const contact = await prisma.contact.create({
         data: {
           email,
           phoneNumber,
-          linkPrecedence: 'primary'
-        }
-      })
+          linkPrecedence: "primary",
+        },
+      });
 
       res.status(200).json({
-        "contact": {
-          "primaryContactId": contact.id,
-          "emails": [contact.email],
-          "phoneNumbers": [contact.phoneNumber],
-          "secondaryContactIds": []
-        }
-      })
+        contact: {
+          primaryContactId: contact.id,
+          emails: [contact.email],
+          phoneNumbers: [contact.phoneNumber],
+          secondaryContactIds: [],
+        },
+      });
 
-      return ;
+      return;
     }
 
     // Last case where we have to Transition from Primary to Secondary
-    const contactWithEmail = contacts.find((contact) => contact.email === email && contact.linkPrecedence === "primary")
-    const contactWithPhoneNumber = contacts.find((contact) => contact.phoneNumber === phoneNumber && contact.linkPrecedence === "primary")
+    const contactWithEmail = contacts.find(
+      (contact) =>
+        contact.email === email && contact.linkPrecedence === "primary"
+    );
+    const contactWithPhoneNumber = contacts.find(
+      (contact) =>
+        contact.phoneNumber === phoneNumber &&
+        contact.linkPrecedence === "primary"
+    );
 
-    if(contactWithEmail && contactWithPhoneNumber) {
+    if (contactWithEmail && contactWithPhoneNumber) {
       await prisma.contact.update({
         where: {
           id: contactWithPhoneNumber.id,
         },
         data: {
           linkPrecedence: "secondary",
-          linkedContactId: contactWithEmail.id
-        }
-      })
+          linkedContactId: contactWithEmail.id,
+        },
+      });
 
       res.status(200).json({
-        "contacts": {
-          "primaryContactId": contactWithEmail.id,
-          "emails": [contactWithEmail.email, contactWithPhoneNumber.email],
-          "phoneNumbers": [contactWithEmail.phoneNumber, contactWithPhoneNumber.phoneNumber],
-          "secondaryContactIds": [contactWithPhoneNumber.id]
-        }
-      })
+        contacts: {
+          primaryContactId: contactWithEmail.id,
+          emails: [contactWithEmail.email, contactWithPhoneNumber.email],
+          phoneNumbers: [
+            contactWithEmail.phoneNumber,
+            contactWithPhoneNumber.phoneNumber,
+          ],
+          secondaryContactIds: [contactWithPhoneNumber.id],
+        },
+      });
 
       return;
     }
 
     // If Phone number is same but the email is different and the contact is secondary and there is only one contact available in database
 
-    if(contacts.length === 1 && contacts[0].linkPrecedence === "secondary") {
-      if(phoneNumber === contacts[0].phoneNumber && email !== contacts[0].email) {
+    if (contacts.length === 1 && contacts[0].linkPrecedence === "secondary") {
+      if (
+        phoneNumber === contacts[0].phoneNumber &&
+        email !== contacts[0].email
+      ) {
         const newContact = await prisma.contact.create({
           data: {
             email,
             phoneNumber,
-            linkPrecedence: 'primary'
-          }
-        })
+            linkPrecedence: "primary",
+          },
+        });
 
         res.status(200).json({
-          "contacts": {
-            "primaryContactId": newContact.id,
-            "emails": [contacts[0].email, newContact.email],
-            "phoneNumbers": [phoneNumber],
-            "secondaryContactIds": [contacts[0].id]
-          }
-        })
+          contacts: {
+            primaryContactId: newContact.id,
+            emails: [contacts[0].email, newContact.email],
+            phoneNumbers: [phoneNumber],
+            secondaryContactIds: [contacts[0].id],
+          },
+        });
 
         return;
-      }
-
-      else if(phoneNumber !== contacts[0].phoneNumber && email === contacts[0].email) {
+      } else if (
+        phoneNumber !== contacts[0].phoneNumber &&
+        email === contacts[0].email
+      ) {
         const newContact = await prisma.contact.create({
           data: {
             email,
             phoneNumber,
-            linkPrecedence: 'primary'
-          }
-        })
+            linkPrecedence: "primary",
+          },
+        });
 
         res.status(200).json({
-          "contacts": {
-            "primaryContactId": newContact.id,
-            "emails": [email],
-            "phoneNumbers": [contacts[0].phoneNumber, newContact.phoneNumber],
-            "secondaryContactIds": [contacts[0].id]
-          }
-        })
+          contacts: {
+            primaryContactId: newContact.id,
+            emails: [email],
+            phoneNumbers: [contacts[0].phoneNumber, newContact.phoneNumber],
+            secondaryContactIds: [contacts[0].id],
+          },
+        });
 
         return;
       }
     }
-    
+
     // if primary contact of the user already exists create secondary contact
     // if contacts are available that means I have to create a secondary contacts
-    // Case 1 - If both email and phoneNumber already exists 
+    // Case 1 - If both email and phoneNumber already exists
     // TODO: Check the case if user can create account using phoneNumber only and email only.
     // TODO: Reduce DB calls
     let isEmailExist = false;
     let isPhoneNumberExists = false;
     let primaryContactId;
-    const emails = new Set();  // created set to store only unique values
+    const emails = new Set(); // created set to store only unique values
     const phoneNumbers = new Set();
     let secondaryContactIds: number[] = [];
 
     contacts.forEach((contact) => {
-      if(contact.email === email) {
+      if (contact.email === email) {
         isEmailExist = true;
       }
 
-      if(contact.phoneNumber === phoneNumber) {
+      if (contact.phoneNumber === phoneNumber) {
         isPhoneNumberExists = true;
       }
 
-      if(contact.linkPrecedence === "primary") {
-        primaryContactId = contact.id
+      if (contact.linkPrecedence === "primary") {
+        primaryContactId = contact.id;
       }
 
-      if(contact.email) {
-        emails.add(contact.email)
+      if (contact.email) {
+        emails.add(contact.email);
       }
 
-      if(contact.phoneNumber) {
-        phoneNumbers.add(contact.phoneNumber)
+      if (contact.phoneNumber) {
+        phoneNumbers.add(contact.phoneNumber);
       }
 
-      if(contact.linkPrecedence === "secondary") {
-        secondaryContactIds.push(contact.id)
+      if (contact.linkPrecedence === "secondary") {
+        secondaryContactIds.push(contact.id);
       }
-    })
+    });
 
-    console.log("details:", isEmailExist, isPhoneNumberExists, primaryContactId)
+    console.log(
+      "details:",
+      isEmailExist,
+      isPhoneNumberExists,
+      primaryContactId
+    );
 
-    if(isEmailExist && isPhoneNumberExists) {
+    if (isEmailExist && isPhoneNumberExists) {
       res.status(200).json({
-        "contacts": {
+        contacts: {
           primaryContactId,
           emails: [...emails],
           phoneNumbers: [...phoneNumbers],
-          secondaryContactIds
-        }
-      })
+          secondaryContactIds,
+        },
+      });
       return;
     }
-    
-    // Case 2: Email not exists 
-    // Added these extra conditions in If because if user doesn't send either of email or password in the payload then you should not create add a row in database. 
-    if(!isEmailExist && isPhoneNumberExists) {
+
+    // Case 2: Email not exists
+    // Added these extra conditions in If because if user doesn't send either of email or password in the payload then you should not create add a row in database.
+    if (!isEmailExist && isPhoneNumberExists) {
       // This check is for, if user doesn't send email in payload.
-      if(email !== undefined) {
-        emails.add(email)
+      if (email !== undefined) {
+        emails.add(email);
       }
 
       let contact;
-      if(email !== undefined) {
+      if (email !== undefined) {
         contact = await prisma.contact.create({
           data: {
             email,
             phoneNumber,
             linkedContactId: primaryContactId,
-            linkPrecedence: "secondary"
-          }
-        })
-        secondaryContactIds.push(contact.id)
+            linkPrecedence: "secondary",
+          },
+        });
+        secondaryContactIds.push(contact.id);
       }
       res.status(200).json({
-        "contacts": {
+        contacts: {
           primaryContactId,
           emails: [...emails],
           phoneNumbers: [...phoneNumbers],
-          secondaryContactIds
-        }
-      })
+          secondaryContactIds,
+        },
+      });
       return;
     }
 
-    // Added these extra conditions in If because if user doesn't send either of email or password in the payload then you should not create add a row in database. 
-    if(isEmailExist && !isPhoneNumberExists) {
-      if(phoneNumber !== undefined) {
-        phoneNumbers.add(phoneNumber)
+    // Added these extra conditions in If because if user doesn't send either of email or password in the payload then you should not create add a row in database.
+    if (isEmailExist && !isPhoneNumberExists) {
+      if (phoneNumber !== undefined) {
+        phoneNumbers.add(phoneNumber);
       }
 
       let contact;
-      if(phoneNumber !== undefined) {
+      if (phoneNumber !== undefined) {
         contact = await prisma.contact.create({
           data: {
             email,
             phoneNumber,
             linkedContactId: primaryContactId,
-            linkPrecedence: "secondary"
-          }
-        })
-        secondaryContactIds.push(contact.id)
+            linkPrecedence: "secondary",
+          },
+        });
+        secondaryContactIds.push(contact.id);
       }
-      
+
       res.status(200).json({
-        "contacts": {
+        contacts: {
           primaryContactId,
           emails: [emails.forEach((email) => emails)],
           phoneNumbers,
-          secondaryContactIds
-        }
-      })
+          secondaryContactIds,
+        },
+      });
       return;
     }
 
-    res.send("hello")
+    res.send("hello");
     return;
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-})
+});
 
-app.listen(3000, () => console.log(`Server started on PORT:${PORT}`))
+app.listen(3000, () => console.log(`Server started on PORT:${PORT}`));
